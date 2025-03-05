8934703eed2fe405891f1dbe69dcb375d78d6818
Yash Wardhan <yashwdan@gmail.com>
2025-03-05 11:15:23 +0530

    Add -HashRoutes resolve a refresh routes

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,31 @@
+// import React from "react";
+// import { BrowserRouter, Route, Routes } from "react-router-dom";
+// import { HelmetProvider } from "react-helmet-async";
+// import Home from "./pages/Home";
+// import About from "./pages/About";
+// import Contact from "./pages/Contact";
+// import Layout from "./Layout";
+
+// const App = () => {
+//   return (
+//     <HelmetProvider>
+//       <BrowserRouter>
+//         <Routes>
+//           <Route path="/" element={<Layout />}>
+//             <Route index element={<Home />} />
+//             <Route path="photos" element={<About />} />
+//             <Route path="contact" element={<Contact />} />
+//           </Route>
+//         </Routes>
+//       </BrowserRouter>
+//     </HelmetProvider>
+//   );
+// };
+
+// export default App;
+
 import React from "react";
-import { BrowserRouter, Route, Routes } from "react-router-dom";
+import { HashRouter, Route, Routes } from "react-router-dom";
 import { HelmetProvider } from "react-helmet-async";
 import Home from "./pages/Home";
 import About from "./pages/About";
@@ -9,7 +35,7 @@ import Layout from "./Layout";
 const App = () => {
   return (
     <HelmetProvider>
-      <BrowserRouter>
+      <HashRouter>
         <Routes>
           <Route path="/" element={<Layout />}>
             <Route index element={<Home />} />
@@ -17,7 +43,7 @@ const App = () => {
             <Route path="contact" element={<Contact />} />
           </Route>
         </Routes>
-      </BrowserRouter>
+      </HashRouter>
     </HelmetProvider>
   );
 };
