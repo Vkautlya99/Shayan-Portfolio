51d647b42de7a64c36cfa5f273fbab6f8591859f
Yash Wardhan <yashwdan@gmail.com>
2025-03-05 17:04:30 +0530

    add sitemap & robot.txt

--- server/index.js
+++ server/index.js
@@ -9,10 +9,8 @@ const __dirname = dirname(__filename);
 
 const app = express();
 
-// Serve static files from the build folder
 app.use(express.static(path.join(__dirname, '..' , 'dist')));
 
-// Catch-all route to handle client-side routing
 app.get('*', (req, res) => {
   res.sendFile(path.join(__dirname, '..','dist', 'index.html'));
 });
