8fc24c4f20ec70167a14ac546f11447dd321980b
Yash Wardhan <yashwdan@gmail.com>
2025-03-06 18:09:04 +0530

    add contact form and emails service

--- server/index.js
+++ server/index.js
@@ -3,17 +3,17 @@ import path from 'path';
 import { fileURLToPath } from 'url';
 import { dirname } from 'path';
 import process from 'process';
+import cors from "cors";
 
-import cors from "cors"
 const app = express();
 app.use(cors());
+app.use(express.json());
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = dirname(__filename);
 
 
 app.use(express.static(path.join(__dirname, '..' , 'dist')));
-
 app.get('*', (req, res) => {
   res.sendFile(path.join(__dirname, '..','dist', 'index.html'));
 });
